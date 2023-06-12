69b018f11c4ef0e65dc00083edb43dd8501be556
mateus-henrique-silva <henrique.s@gsuite.iff.edu.br>
2023-06-12 15:41:05 -0300

    .

--- src/router/rotas/rotas.go
+++ src/router/rotas/rotas.go
@@ -1,10 +1,23 @@
 package rotas
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Rota struct {
 	Uri                 string
 	Metodo              string
 	Funcao              func(w http.ResponseWriter, r *http.Request)
 	RequerAutentication bool
+}
+
+// configurar rotas
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := usuariosRotas
+	for _, rota := range rotas {
+		r.HandleFunc(rota.Uri, rota.Funcao).Methods(rota.Metodo)
+	}
+	return r
 }
--- src/router/router.go
+++ src/router/router.go
@@ -1,7 +1,11 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"go.mod/src/router/rotas"
+)
 
 func Gerar() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()
+	return rotas.Configurar(r)
 }
