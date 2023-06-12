705b705067c390b7a60ca74d69b0c0fa5ca17ffb
mateus-henrique-silva <henrique.s@gsuite.iff.edu.br>
2023-06-12 15:32:50 -0300

    ajustando rotas

--- src/router/rotas/usuarios.go
+++ src/router/rotas/usuarios.go
@@ -13,23 +13,23 @@ var usuariosRotas = []Rota{
 	},
 	{
 		Uri:    "usarios",
-		Metodo: http.MethodPost,
+		Metodo: http.MethodGet,
 		Funcao: func(w http.ResponseWriter, r *http.Request) {
 
 		},
 		RequerAutentication: false,
 	},
 	{
-		Uri:    "usarios",
-		Metodo: http.MethodPost,
+		Uri:    "/{id}/usarios",
+		Metodo: http.MethodGet,
 		Funcao: func(w http.ResponseWriter, r *http.Request) {
 
 		},
 		RequerAutentication: false,
 	},
 	{
-		Uri:    "usarios",
-		Metodo: http.MethodPost,
+		Uri:    "/{id}/usarios",
+		Metodo: http.MethodPut,
 		Funcao: func(w http.ResponseWriter, r *http.Request) {
 
 		},
