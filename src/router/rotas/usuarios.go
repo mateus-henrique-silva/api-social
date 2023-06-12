59411c6bbac3e51f1f62c44d71ab925883eb94a1
mateus-henrique-silva <henrique.s@gsuite.iff.edu.br>
2023-06-12 15:24:49 -0300

    criando rotas

---
+++ src/router/rotas/usuarios.go
@@ -0,0 +1,14 @@
+package rotas
+
+import "net/http"
+
+var usuariosRotas = []Rota{
+	{
+		Uri:    "usarios",
+		Metodo: http.MethodPost,
+		Funcao: func(w http.ResponseWriter, r *http.Request) {
+
+		},
+		RequerAutentication: false,
+	},
+}
